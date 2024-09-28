4fe2f09f93a4e96ecb18d3049a38499c7789b55a
youyou4444821 <70963556+youyou4444821@users.noreply.github.com>
2024-09-28 09:45:37 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+{
+  const btn = document.getElementById('btn');
+
+  const result = document.getElementById('result');
+
+  btn.addEventListener('click',()=>{
+    const results = ['大吉','中吉','凶','小吉','吉','大凶'];
+    const n = Math.floor(Math.random()*results.length);
+    result.textContent = results[n];
+
+      
+
+  });
+}
